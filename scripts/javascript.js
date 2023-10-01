439433fa4a3dd246fdb3aa3b2988697c33cfb738
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2023-10-01 18:49:59 +0200

    Added working code that makes you colour divs as you hover them

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -1,11 +1,22 @@
 let div = document.createElement('div');
-div.classList.add("colored-div");
+
 for ( let i = 0; i < 32; i++ ) {
     div = document.createElement("div");
-    div.classList.add("colored-div");
     container.appendChild(div);
+    div.classList.add("colored-div", "hover");
+    
 }
+   
+/*div.addEventListener("mouseover", function (e) {
+    div.style.backgroundColor = "black";
+ });*/
 
-//let hover = document.querySelector(".colored-div");
+
+let hover = document.querySelectorAll(".hover");
+hover.forEach(function (element) {
+    element.addEventListener("mouseover", function (e) {
+        element.style.backgroundColor = "black";
+    });
+});
 //hover.onmouseover = () => div.cssText = "background-color: black";
 //hover.onmouseout = () => div.cssText = "background-color: black";
