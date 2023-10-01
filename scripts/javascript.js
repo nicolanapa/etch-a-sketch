4d37ec1a148a7ad53e1676149a8bc41e654ac1ff
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2023-10-01 20:18:49 +0200

    Added algorithm that checks if you have clicked once or two times the button and then refresh the page

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -1,28 +1,39 @@
 let div = document.createElement('div');
-
 let popup = document.querySelector("#popup");
+let touched = false;
 popup.addEventListener("click", () => {
-    prompt("Please enter how much grid size you want", "");
-
-})
-
-for ( let i = 0; i < (16*16); i++ ) {
+    if (touched == true) {
+        location.reload();
+    }
+    touched = true;
+    let pronto = prompt("Please enter how much grid size you want", "16");
+    
+    if ( pronto > 100 ) {
+        return alert("Can't output more than 100x100 grids!");
+    }
+    if ( pronto < 0 ) {
+        return alert("Can't output negative numbers!");
+    }
+    let ii = pronto;
+    ii = ii * ii;
+    for ( let i = 0; i < (ii - 1); i++ ) {
     div = document.createElement("div");
     container.appendChild(div);
-    div.classList.add("colored-div", "hover");
-    
-}
+    div.classList.add("colored-div", "hover"); 
+    }
+    let hover = document.querySelectorAll(".hover");
+    hover.forEach(function (element) {
+    element.addEventListener("mouseover", function (e) {
+        element.style.backgroundColor = "black";
+        pronto = parseInt(pronto);
+    });
+});
+});
+
+
    
 /*div.addEventListener("mouseover", function (e) {
     div.style.backgroundColor = "black";
  });*/
-
-
-let hover = document.querySelectorAll(".hover");
-hover.forEach(function (element) {
-    element.addEventListener("mouseover", function (e) {
-        element.style.backgroundColor = "black";
-    });
-});
 //hover.onmouseover = () => div.cssText = "background-color: black";
 //hover.onmouseout = () => div.cssText = "background-color: black";
