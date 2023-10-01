9886918225656be87bd230573593704de62728c9
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2023-10-01 17:01:22 +0200

    Added bugged code 2

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -1,6 +1,11 @@
 let div = document.createElement('div');
+div.classList.add("colored-div");
 for ( let i = 0; i < 32; i++ ) {
     div = document.createElement("div");
-    div.style.cssText = "display: flex; background-color: white; border: 1px solid grey;";
+    div.classList.add("colored-div");
     container.appendChild(div);
 }
+
+//let hover = document.querySelector(".colored-div");
+//hover.onmouseover = () => div.cssText = "background-color: black";
+//hover.onmouseout = () => div.cssText = "background-color: black";
